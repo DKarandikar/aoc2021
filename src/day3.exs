80c11f38380c809fe192d44ca4b25f9ace4e6814
DKarandikar <daniel.karandikar@zoho.com>
2021-12-03 11:07:35 +0000

    Refactor a bit

--- src/day3.exs
+++ src/day3.exs
@@ -1,69 +1,62 @@
 
 defmodule Day3 do
-  def size() do
-    11
+  def range(lines) do
+    0 .. (String.length(hd lines) - 1)
   end
 
   def getLines() do
     File.read!("input/day3.txt") |> String.split("\n")
   end
 
+  @doc """
+  Get the bit which satisfies `f(total, length(lines)/2)` for column `index` of `lines`
+  """
+  def get_filtered_bit(index, lines, f) do
+    v = Enum.map(lines, fn line -> if String.at(line, index) == "1", do: 1, else: 0 end)
+    if f.(Enum.sum(v), length(lines) /2), do: "1", else: "0"
+  end
+
   def solvePartA() do
     lines = Day3.getLines
-    res = Enum.reduce(lines, Enum.map(0..Day3.size, fn x -> 0 end), &Day3.acca/2)
-    gamma = Enum.map(0..Day3.size, fn x -> if Enum.at(res, x) > length(lines)/2, do: "1", else: "0" end)
-    epsilon = Enum.map(0..Day3.size, fn x -> if Enum.at(gamma, x) == "1", do: "0", else: "1" end)
 
-    IO.inspect(res)
-    IO.inspect(gamma)
-    IO.inspect(epsilon)
+    # The below generation of gamma is less efficient than summing all bits in one pass, then
+    # generating based on those that are greater than len(lines)/2; but this is neat
+    gamma = Enum.map(Day3.range(lines), fn x -> get_filtered_bit(x, lines, &(&1 >= &2)) end)
+
+    epsilon = Enum.map(gamma, fn x -> if x == "1", do: "0", else: "1" end)
 
     g = String.to_integer(List.to_string(gamma), 2)
     e = String.to_integer(List.to_string(epsilon), 2)
-    g * e
-  end
 
-  def acca(line, acc) do
-    v = Enum.map(0..Day3.size, fn x -> if String.at(line, x) == "1", do: 1, else: 0 end)
-    Enum.map(Enum.zip(acc, v), fn x -> elem(x, 0) + elem(x, 1) end)
+    g * e
   end
 
   def solvePartB() do
     lines = Day3.getLines
 
-    oxygen = Enum.reduce_while(0..Day3.size, lines, &Day3.accb/2)
-    co2 = Enum.reduce_while(0..Day3.size, lines, &Day3.accb2/2)
+    oxygen = do_bitwise_filtering(lines, &(&1 >= &2)) |> String.to_integer(2)
+    co2 = do_bitwise_filtering(lines, &(&1 < &2)) |> String.to_integer(2)
 
-    g = String.to_integer(oxygen, 2)
-    e = String.to_integer(co2, 2)
-    g * e
+    oxygen * co2
   end
 
-  def accb(index, lines) do
-    most_common = Day3.get_most_common(index, lines)
-    new_lines = Enum.filter(lines, fn line -> String.at(line, index) == most_common end)
+  @doc """
+  Take a list of lines which should be strings of 1s and 0s
+  Take each column at a time, get the total number of 1s and keep lines
+  for which `f(total, length(lines)/2)` is true
+  Do this recursively until only one line is left
+  """
+  def do_bitwise_filtering(lines, f) do
+    Enum.reduce_while(Day3.range(lines), lines, fn index, lines_acc ->
+      column_bit_of_choice = Day3.get_filtered_bit(index, lines_acc, f)
+      new_lines = Enum.filter(lines_acc, fn line -> String.at(line, index) == column_bit_of_choice end)
 
-    if length(new_lines) == 1, do: {:halt, Enum.at(new_lines, 0)}, else: {:cont, new_lines}
-  end
-
-  def accb2(index, lines) do
-    least_common = Day3.get_least_common(index, lines)
-    new_lines = Enum.filter(lines, fn line -> String.at(line, index) == least_common end)
-
-    if length(new_lines) == 1, do: {:halt, Enum.at(new_lines, 0)}, else: {:cont, new_lines}
-  end
-
-  def get_most_common(index, lines) do
-    v = Enum.map(lines, fn line -> if String.at(line, index) == "1", do: 1, else: 0 end)
-    if Enum.sum(v) >= length(lines) /2, do: "1", else: "0"
-  end
-
-  def get_least_common(index, lines) do
-    v = Enum.map(lines, fn line -> if String.at(line, index) == "1", do: 1, else: 0 end)
-    if Enum.sum(v) < length(lines) /2, do: "1", else: "0"
+      if length(new_lines) == 1, do: {:halt, Enum.at(new_lines, 0)}, else: {:cont, new_lines}
+    end
+    )
   end
 
 end
 
-# IO.inspect(Day3.solvePartA())
+IO.puts(Day3.solvePartA())
 IO.puts(Day3.solvePartB())
