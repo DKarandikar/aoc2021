c562f9d43f82395d14c180cdef00128f05024b2e
DKarandikar <daniel.karandikar@zoho.com>
2021-12-06 08:51:39 +0000

    Do day 6
    Pleased with this one, didn't need to change for part 2
    Mainting a map of ages to numbers works v. efficiently

--- src/day6.exs
+++ src/day6.exs
@@ -3,14 +3,43 @@ defmodule Day6 do
     File.read!("input/day6.txt") |> String.split("\n")
   end
 
+  def incrementMapValue(map, key, val) do
+    Map.put(map, key, val + Map.get(map, key, 0))
+  end
+
+  def simulateFish(num_days) do
+    Day6.getLines
+      |> hd
+      |> String.split(",")
+      |> Enum.map(&String.to_integer/1)
+      |> Enum.reduce(%{}, fn i, map -> Day6.incrementMapValue(map, i, 1) end)
+      |> runFishSimulation(num_days)
+      |> sumFish
+  end
+
+  def runFishSimulation(fish, num_days) do
+    Enum.reduce(1..num_days, fish, fn _, m -> Day6.doDay(m) end)
+  end
+
+  def doDay(m) do
+    { new_births, new_map } = Enum.reduce(Map.keys(m), %{}, fn i, map -> Map.put(map, (i-1), Map.get(m, i)) end)
+      |> Map.pop(-1, 0)
+
+    new_map
+      |> Day6.incrementMapValue(6, new_births)
+      |> Day6.incrementMapValue(8, new_births)
+  end
+
+  def sumFish(m) do
+    Enum.sum(Map.values(m))
+  end
+
   def solvePartA() do
-    lines = Day6.getLines
-    lines |> hd
+    simulateFish(80)
   end
 
   def solvePartB() do
-    lines = Day6.getLines
-    lines |> hd
+    simulateFish(256)
   end
 
 end
