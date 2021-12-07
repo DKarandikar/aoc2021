edcefd11afc8863c99b41f106e42d9f20bc16c32
DKarandikar <daniel.karandikar@zoho.com>
2021-12-07 08:35:50 +0000

    Solve day 7

--- src/day7.exs
+++ src/day7.exs
@@ -1,16 +1,36 @@
 defmodule Day7 do
-  def getLines() do
+  def getNums() do
     File.read!("input/day7.txt") |> String.split("\n")
+      |> hd
+      |> String.split(",")
+      |> Enum.map(&String.to_integer/1)
+  end
+
+  def getMinMovement(cost_fn) do
+    nums = getNums()
+
+    nums
+      |> (&(Enum.min(&1)..Enum.max(&1))).()
+      |> Enum.map(
+        fn i ->
+          nums
+            |> Enum.map(fn n -> cost_fn.(abs(n-i)) end)
+            |> Enum.sum()
+        end
+        )
+      |> Enum.min()
   end
 
   def solvePartA() do
-    lines = Day7.getLines
-    lines |> hd
+    getMinMovement(&(&1))
   end
 
   def solvePartB() do
-    lines = Day7.getLines
-    lines |> hd
+    getMinMovement(&(genCost(&1)))
+  end
+
+  def genCost(x) do
+    if x == 0, do: 0, else: Enum.sum(1..x)
   end
 
 end
