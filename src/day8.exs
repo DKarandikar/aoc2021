66127ab9ca98aa55bcd2567977a3f235fde7d7ee
DKarandikar <daniel.karandikar@zoho.com>
2021-12-08 08:20:52 +0000

    Do day 8 part A

--- src/day8.exs
+++ src/day8.exs
@@ -1,11 +1,20 @@
 defmodule Day8 do
   def getLines() do
-    File.read!("input/day8.txt") |> String.split("\n")
+    lines = File.read!("input/day8.txt")
+    |> String.split("\n")
+
+    Enum.map(lines, fn x -> String.split(x, "|") end)
   end
 
   def solvePartA() do
     lines = Day8.getLines
-    lines |> hd
+    Enum.reduce(lines, 0, fn line, acc ->
+      [input, output] = line
+      u = String.split(String.trim(output), " ")
+        |> Enum.filter(fn x -> String.length(x) == 3 || String.length(x) == 7 || String.length(x) == 2 || String.length(x) == 4 end )
+        |> length()
+      acc + u
+    end)
   end
 
   def solvePartB() do
@@ -15,5 +24,5 @@ defmodule Day8 do
 
 end
 
-IO.puts(Day8.solvePartA())
-IO.puts(Day8.solvePartB())
+IO.inspect(Day8.solvePartA())
+# IO.puts(Day8.solvePartB())
