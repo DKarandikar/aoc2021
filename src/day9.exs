79498242d7375fc2f2f51009d8070007268d7a6c
DKarandikar <daniel.karandikar@zoho.com>
2021-12-09 08:37:01 +0000

    Do day 9 part 1

--- src/day9.exs
+++ src/day9.exs
@@ -5,7 +5,29 @@ defmodule Day9 do
 
   def solvePartA() do
     lines = Day9.getLines
-    lines |> hd
+    array = lines |> Enum.map(fn x -> x |> String.graphemes() |> Enum.map(&String.to_integer/1) end)
+
+    maxX = length(array |> hd)
+    maxY = length(array)
+
+    pos = [{0, 1}, {0, -1}, {1, 0}, {-1, 0}]
+
+    Enum.reduce(0.. maxY - 1, [], fn y, acc ->
+      Enum.reduce(0 .. maxX - 1, acc, fn x, acc2 ->
+        loc_value = array |> Enum.at(y) |> Enum.at(x)
+
+        coords_inside = Enum.map(pos, fn move -> {x + elem(move, 0), y + elem(move, 1)} end)
+          |> Enum.filter(fn {x1, y1} -> x1 > -1 && y1 > -1 && x1 < maxX && y1 < maxY end)
+          |> Enum.map(fn {x1, y1} -> Enum.at(array, y1) |> Enum.at(x1) end)
+
+        if length(coords_inside) == length(Enum.filter(coords_inside, fn val -> val > loc_value end)), do:
+          [loc_value | acc2],
+        else:
+          acc2
+      end)
+    end )
+    |> Enum.map(fn x -> x + 1 end)
+    |> Enum.sum()
   end
 
   def solvePartB() do
@@ -15,5 +37,5 @@ defmodule Day9 do
 
 end
 
-IO.puts(Day9.solvePartA())
-IO.puts(Day9.solvePartB())
+IO.inspect(Day9.solvePartA())
+# IO.puts(Day9.solvePartB())
