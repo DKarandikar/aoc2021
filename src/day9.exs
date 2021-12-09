98f2cdb80a5cbf5f3321cefbbe6c21da714144e9
DKarandikar <daniel.karandikar@zoho.com>
2021-12-09 11:25:22 +0000

    Dry out low points code

--- src/day9.exs
+++ src/day9.exs
@@ -3,47 +3,19 @@ defmodule Day9 do
     File.read!("input/day9.txt") |> String.split("\n")
   end
 
-  def solvePartA() do
-    lines = Day9.getLines
-    array = lines |> Enum.map(fn x -> x |> String.graphemes() |> Enum.map(&String.to_integer/1) end)
+  def positions() do
+    [{0, 1}, {0, -1}, {1, 0}, {-1, 0}]
+  end
 
+  def getLowPoints(array) do
     maxX = length(array |> hd)
     maxY = length(array)
-
-    pos = [{0, 1}, {0, -1}, {1, 0}, {-1, 0}]
 
     Enum.reduce(0.. maxY - 1, [], fn y, acc ->
       Enum.reduce(0 .. maxX - 1, acc, fn x, acc2 ->
         loc_value = array |> Enum.at(y) |> Enum.at(x)
 
-        coords_inside = Enum.map(pos, fn move -> {x + elem(move, 0), y + elem(move, 1)} end)
-          |> Enum.filter(fn {x1, y1} -> x1 > -1 && y1 > -1 && x1 < maxX && y1 < maxY end)
-          |> Enum.map(fn {x1, y1} -> Enum.at(array, y1) |> Enum.at(x1) end)
-
-        if length(coords_inside) == length(Enum.filter(coords_inside, fn val -> val > loc_value end)), do:
-          [loc_value | acc2],
-        else:
-          acc2
-      end)
-    end )
-    |> Enum.map(fn x -> x + 1 end)
-    |> Enum.sum()
-  end
-
-  def solvePartB() do
-    lines = Day9.getLines
-    array = lines |> Enum.map(fn x -> x |> String.graphemes() |> Enum.map(&String.to_integer/1) end)
-
-    maxX = length(array |> hd)
-    maxY = length(array)
-
-    pos = [{0, 1}, {0, -1}, {1, 0}, {-1, 0}]
-
-    low_points = Enum.reduce(0.. maxY - 1, [], fn y, acc ->
-      Enum.reduce(0 .. maxX - 1, acc, fn x, acc2 ->
-        loc_value = array |> Enum.at(y) |> Enum.at(x)
-
-        coords_inside = Enum.map(pos, fn move -> {x + elem(move, 0), y + elem(move, 1)} end)
+        coords_inside = Enum.map(positions(), fn move -> {x + elem(move, 0), y + elem(move, 1)} end)
           |> Enum.filter(fn {x1, y1} -> x1 > -1 && y1 > -1 && x1 < maxX && y1 < maxY end)
           |> Enum.map(fn {x1, y1} -> Enum.at(array, y1) |> Enum.at(x1) end)
 
@@ -53,28 +25,37 @@ defmodule Day9 do
           acc2
       end)
     end )
+  end
 
-    # IO.inspect(low_points)
+  def solvePartA() do
+    lines = Day9.getLines
+    array = lines |> Enum.map(fn x -> x |> String.graphemes() |> Enum.map(&String.to_integer/1) end)
 
-    rv = Enum.map(low_points, fn x -> findBasinSize(array, x) end)
+    array
+      |> getLowPoints()
+      |> Enum.map(fn {x, y} -> array |> Enum.at(y) |> Enum.at(x) end)
+      |> Enum.map(fn x -> x + 1 end)
+      |> Enum.sum()
+  end
+
+  def solvePartB() do
+    lines = Day9.getLines
+    array = lines |> Enum.map(fn x -> x |> String.graphemes() |> Enum.map(&String.to_integer/1) end)
+
+    rv = array
+      |> getLowPoints()
+      |> Enum.map(fn x -> findBasinSize(array, x) end)
       |> Enum.map(&MapSet.size/1)
       |> Enum.sort(&>=/2)
 
     Enum.at(rv, 0) * Enum.at(rv, 1) * Enum.at(rv, 2)
 
-
   end
 
-  def findBasinSize(array, loc) do
-    explorePos(array, loc)
-  end
-
-  def explorePos(array, {x, y}) do
+  def findBasinSize(array, {x, y}) do
     Enum.reduce_while(Stream.cycle([0]), {[{x, y}], MapSet.new()}, fn _, {sofar, done} ->
 
       if length(sofar) == 0, do: {:halt, done}, else: (
-
-        # IO.inspect({sofar, done})
 
         [{x, y} | sofar] = sofar
         done = MapSet.put(done, {x, y})
@@ -85,15 +66,9 @@ defmodule Day9 do
         |> Enum.filter(fn {x1, y1} -> not MapSet.member?(MapSet.union(MapSet.new(sofar), done), {x1, y1}) end)
         |> Enum.filter(fn {x1, y1} -> array |> Enum.at(y1) |> Enum.at(x1) != 9 end)
 
-        # IO.inspect(new_coords)
-
-        sofar = Enum.concat(sofar, new_coords)
-
-        {:cont, {sofar, done}}
+        {:cont, {Enum.concat(sofar, new_coords), done}}
 
       )
-
-
     end)
   end
 end
