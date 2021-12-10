b8041ee482a07048ef075c4e8a01985a4de754f3
DKarandikar <daniel.karandikar@zoho.com>
2021-12-10 08:22:40 +0000

    Do day 10 part 1

--- src/day10.exs
+++ src/day10.exs
@@ -5,7 +5,43 @@ defmodule Day10 do
 
   def solvePartA() do
     lines = Day10.getLines
-    lines |> hd
+
+    lines
+      |> Enum.map(&getCorruptChar/1)
+      |> Enum.map(&getScore/1)
+      |> Enum.sum()
+
+  end
+
+  def getScore(char) do
+    cond do
+      char == ">" -> 25137
+      char == "}" -> 1197
+      char == "]" -> 57
+      char == ")" -> 3
+      true -> 0
+    end
+  end
+
+  def getCorruptChar(line) do
+    opening = MapSet.new(["(", "<", "{", "["])
+    closing = MapSet.new([")", ">", "}", "]"])
+    pairs = %{"(" => ")", "[" => "]", "<" => ">", "{" => "}"}
+
+    r = Enum.reduce_while(String.graphemes(line), [], fn c, acc ->
+      if MapSet.member?(opening, c), do: (
+        {:cont, [c | acc]}
+      ), else: (
+        [mostRecent | t] = acc
+        if Map.fetch!(pairs, mostRecent) != c, do: {:halt, c}, else: {:cont, t}
+      )
+    end)
+
+    cond do
+      is_list(r) -> nil
+      true -> r
+    end
+
   end
 
   def solvePartB() do
@@ -16,4 +52,4 @@ defmodule Day10 do
 end
 
 IO.puts(Day10.solvePartA())
-IO.puts(Day10.solvePartB())
+# IO.puts(Day10.solvePartB())
