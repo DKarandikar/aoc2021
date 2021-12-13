5ed8c65fa1312ffe88f359df82c2de080021f44a
DKarandikar <daniel.karandikar@zoho.com>
2021-12-13 08:44:49 +0000

    Solve day 13, part 1

--- src/day13.exs
+++ src/day13.exs
@@ -1,11 +1,42 @@
 defmodule Day13 do
-  def getLines() do
-    File.read!("input/day13.txt") |> String.split("\n")
+  def getFolds() do
+    File.read!("input/day13fold.txt")
+      |> String.split("\n")
+      |> Enum.map(fn x -> String.split(x, "fold along ") end)
+      |> Enum.map(fn x -> Enum.at(x, 1) end)
+      |> Enum.map(fn x -> String.split(x, "=") end)
+      |> Enum.map(fn x -> {Enum.at(x, 0), String.to_integer(Enum.at(x, 1))} end)
+  end
+
+  def getDots() do
+    File.read!("input/day13dot.txt") |> String.split("\n")
+      |> Enum.map(fn x -> String.split(x, ",") end)
+      |> Enum.map(fn x -> {String.to_integer(Enum.at(x, 0)), String.to_integer(Enum.at(x, 1))} end)
+      |> MapSet.new()
   end
 
   def solvePartA() do
-    lines = Day13.getLines
-    lines |> hd
+    dots = getDots()
+    first_fold = getFolds() |> hd
+    doFold(dots, first_fold) |> MapSet.size()
+  end
+
+  def doFold(dots, {axis, number}) do
+    loc = if axis == "y", do: 1, else: 0
+
+    Enum.reduce(dots, MapSet.new(), fn dot, acc ->
+      if elem(dot, loc) < number, do: (
+        MapSet.put(acc, dot)
+        ), else: (
+          {x, y} = dot
+          if loc == 1, do: (
+            MapSet.put(acc, {x, (2*number) - y})
+          ), else: (
+            MapSet.put(acc, {(2*number) - x, y})
+          )
+      )
+    end)
+
   end
 
   def solvePartB() do
@@ -15,5 +46,5 @@ defmodule Day13 do
 
 end
 
-IO.puts(Day13.solvePartA())
-IO.puts(Day13.solvePartB())
+IO.inspect(Day13.solvePartA())
+# IO.puts(Day13.solvePartB())
