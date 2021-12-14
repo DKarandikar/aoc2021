cfc8e83c5fe8360bc8292f231577b72590344981
DKarandikar <daniel.karandikar@zoho.com>
2021-12-14 09:57:20 +0000

    Do day 14 part 2
    Improve algorithm significantly
    Just track pairs

--- src/day14.exs
+++ src/day14.exs
@@ -11,47 +11,56 @@ defmodule Day14 do
     {starter, rules}
   end
 
-  def incrementMapValue(map, key) do
-    Map.put(map, key, 1 + Map.get(map, key, 0))
-  end
+  def incrementMapValue(map, key, val), do: Map.put(map, key, val + Map.get(map, key, 0))
+  def getVal(m), do: Enum.max(Map.values(m)) - Enum.min(Map.values(m))
 
-  def doStep({polymer, rules}) do
-    pairs = for i <- 0 .. (String.length(polymer) - 2), into: [], do: Map.get(rules, String.slice(polymer, i, 2), "")
-    pairs = pairs ++ [""]
-
-    res = Enum.reduce(0 .. (String.length(polymer) -1), "", fn i, acc ->
-      acc <> String.slice(polymer, i, 1) <> Enum.at(pairs, i)
-    end)
-  end
-
-  def countLetters(s) do
-    Enum.reduce( 0 .. String.length(s) - 1, %{}, fn i, acc ->
-      incrementMapValue(acc, String.slice(s, i, 1))
-    end)
-  end
-
-  def getVal(map) do
-    Enum.max(Map.values(map)) - Enum.min(Map.values(map))
-  end
-
-  def solvePartA() do
+  def runSimulation(steps) do
     {start, rules} = Day14.parseInput
+    start = processStarter(start)
 
-    res = Enum.reduce(0 .. 9, start, fn _, acc ->
-      doStep({acc, rules})
+    Enum.reduce(0 .. steps - 1, start, fn _, acc ->
+      applyStep(acc, rules)
+    end)
+      |> countLettersMap()
+      |> getVal()
+  end
+
+  def processStarter(s) do
+    Enum.reduce( 0 .. String.length(s) - 2, %{}, fn i, acc ->
+      incrementMapValue(acc, String.slice(s, i, 2), 1)
+    end)
+  end
+
+  def applyStep(pairSet, rules) do
+    Enum.reduce(Map.keys(pairSet), %{}, fn key, acc ->
+      insert = Map.get(rules, key, nil)
+      if insert == nil, do: acc, else: (
+
+        new_1 = String.slice(key, 0, 1) <> insert
+        new_2 = insert <> String.slice(key, 1, 1)
+
+        acc = incrementMapValue(acc, new_1, Map.get(pairSet, key, 0))
+        incrementMapValue(acc, new_2, Map.get(pairSet, key, 0))
+      )
+    end)
+  end
+
+  def countLettersMap(m) do
+    start_letters = Enum.reduce( Map.keys(m), %{}, fn key, acc ->
+      incrementMapValue(acc, String.slice(key, 0, 1), Map.get(m, key, 0))
     end)
 
-    countLetters(res)
-     |> getVal()
+    end_letters = Enum.reduce( Map.keys(m), %{}, fn key, acc ->
+      incrementMapValue(acc, String.slice(key, 1, 1), Map.get(m, key, 0))
+    end)
+
+    Map.merge(start_letters, end_letters, fn _, x, y -> max(x, y) end)
   end
 
-
-  def solvePartB() do
-    lines = Day14.getLines
-    lines |> hd
-  end
+  def solvePartA(), do: runSimulation(10)
+  def solvePartB(), do: runSimulation(40)
 
 end
 
 IO.puts(Day14.solvePartA())
-# IO.puts(Day14.solvePartB())
+IO.puts(Day14.solvePartB())
