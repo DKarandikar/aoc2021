64ac3c741f885a175e5fa3071ffebdd09c34c318
DKarandikar <daniel.karandikar@zoho.com>
2021-12-16 15:51:56 +0000

    Day 16 taking this slow to use bit strings
    Parse literal packets

--- src/day16.exs
+++ src/day16.exs
@@ -5,8 +5,15 @@ defmodule Day16 do
 
   def solvePartA() do
     lines = Day16.getLines
-    lines |> hd
+    lines |> hd |> Base.decode16!() |> packet()
   end
+
+  def packet(<<version::size(3), type::size(3), rem::bitstring>>) when type == 4 do
+     literal(rem, 0, version)
+  end
+
+  def literal(<<0::1, value::size(4), rem::bitstring>>, acc, v), do: {rem, acc * 16 + value, v}
+  def literal(<<1::1, value::size(4), rem::bitstring>>, acc, v), do: literal(rem, acc * 16 + value, v)
 
   def solvePartB() do
     lines = Day16.getLines
@@ -15,5 +22,5 @@ defmodule Day16 do
 
 end
 
-IO.puts(Day16.solvePartA())
-IO.puts(Day16.solvePartB())
+IO.inspect(Day16.solvePartA())
+IO.inspect(Day16.solvePartB())
