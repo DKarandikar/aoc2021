36b147383adfd5107042edb62d55fa8a66168b63
DKarandikar <daniel.karandikar@zoho.com>
2021-12-16 16:56:09 +0000

    Fixup ordering of packets and operators

--- src/day16.exs
+++ src/day16.exs
@@ -4,11 +4,11 @@ defmodule Day16 do
   end
 
   def solvePartA() do
-    lines = Day16.getLines |> hd |> Base.decode16!() |> packet() |> elem(2)
+    Day16.getLines |> hd |> Base.decode16!() |> packet() |> elem(2)
   end
 
   def solvePartB() do
-    lines = Day16.getLines |> hd |> Base.decode16!() |> packet() |> elem(1)
+    Day16.getLines |> hd |> Base.decode16!() |> packet() |> elem(1)
   end
 
   def packet(<<version::size(3), type::size(3), rem::bitstring>>) when type == 4 do
@@ -31,14 +31,14 @@ defmodule Day16 do
 
   def packetType0(bits, packet_list, version_acc, final_remainder) do
     {remainder, output, version} = packet(bits)
-    packetType0(remainder, [output | packet_list], version_acc + version, final_remainder)
+    packetType0(remainder, packet_list ++ [output], version_acc + version, final_remainder)
   end
 
   def packetType1(bits, packet_list, 0, version_acc), do: {bits, packet_list, version_acc}
 
   def packetType1(bits, packet_list, n, version_acc) do
     {remainder, output, version} = packet(bits)
-    packetType1(remainder, [output | packet_list], n - 1, version_acc + version)
+    packetType1(remainder, packet_list ++ [output], n - 1, version_acc + version)
   end
 
   def literal(<<0::1, value::size(4), rem::bitstring>>, acc, v), do: {rem, acc * 16 + value, v}
@@ -49,8 +49,8 @@ defmodule Day16 do
   def calc(nums, 2), do: Enum.min(nums)
   def calc(nums, 3), do: Enum.max(nums)
 
-  def calc([a, b], 5), do: if a < b, do: 1, else: 0
-  def calc([a, b], 6), do: if a > b, do: 1, else: 0
+  def calc([a, b], 5), do: if a > b, do: 1, else: 0
+  def calc([a, b], 6), do: if a < b, do: 1, else: 0
   def calc([a, b], 7), do: if a == b, do: 1, else: 0
 
 end
