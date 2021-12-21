2b5647b03ffd55307d31228323c75ac20e06d668
DKarandikar <daniel.karandikar@zoho.com>
2021-12-21 11:53:38 +0000

    Do day 21 part 2
    Really slow (a few minutes)
    But it does work, adding the sort for the quantum step helped a lot
    I'm sure there are other things like that to speed this up

--- src/day21.exs
+++ src/day21.exs
@@ -6,6 +6,8 @@ defmodule Day21 do
       |> Enum.map(fn x -> String.to_integer(x) end)
   end
 
+  def incrementMapValue(map, key, val), do: Map.put(map, key, val + Map.get(map, key, 0))
+
   def get_roll(n) do
     Enum.sum(Enum.map([0, 1, 2], fn x -> rem((n*3 + x), 100) + 1 end))
   end
@@ -36,11 +38,59 @@ defmodule Day21 do
 
   end
 
+  def quantum_roll() do
+    # roll_score -> num universes
+    %{3 => 1, 4 => 3, 5 => 6, 6 => 7, 7 => 6, 8 => 3, 9 => 1}
+  end
+
+  def do_quantum_step({p1_wins, p2_wins, universes}) do
+    {p1pos, p2pos, p1s, p2s, p1_roll} = Map.keys(universes) |> Enum.sort(&(elem(&1, 3) <= elem(&2, 3))) |> Enum.at(0)
+    IO.inspect({p1s, p2s})
+    {num, universes} = Map.pop(universes, {p1pos, p2pos, p1s, p2s, p1_roll})
+
+    rolls = quantum_roll()
+
+    Enum.reduce(Map.keys(rolls), {p1_wins, p2_wins, universes}, fn roll_score, {p1w, p2w, us} ->
+      # p1 to roll on even
+      if p1_roll, do: (
+        p1pos = do_move(p1pos, roll_score)
+        p1s = p1s + p1pos
+        num_universes = num * Map.get(rolls, roll_score)
+
+        if p1s > 20, do: {p1w + num_universes, p2w, us}, else: {p1w, p2w, incrementMapValue(us, {p1pos, p2pos, p1s, p2s, false}, num_universes)}
+      ), else: (
+
+        p2pos = do_move(p2pos, roll_score)
+        p2s = p2s + p2pos
+        num_universes = num * Map.get(rolls, roll_score)
+
+        if p2s > 20, do: (
+          {p1w, p2w  + num_universes, us}
+        ), else: {p1w, p2w, incrementMapValue(us, {p1pos, p2pos, p1s, p2s, true}, num_universes)}
+      )
+    end)
+
+  end
+
   def solvePartB() do
-    lines = Day21.getLines
-    lines |> hd
+    [p1, p2] = getLines()
+
+    universes = %{{p1, p2, 0, 0, true} => 1}  # p1 pos, p2 pos, p1 score, p2 score, p1_roll
+
+    state = {0, 0, universes}
+
+    do_quantum_step(state)
+
+    Enum.reduce_while(Stream.cycle([0]), state, fn _, state ->
+      {p1, p2, us} = do_quantum_step(state)
+      if Map.size(us) == 0, do: {:halt, {p1, p2}}, else: (
+        # IO.puts(Map.size(us))
+        {:cont, {p1, p2, us}}
+      )
+    end)
   end
 
 end
 
 IO.inspect(Day21.solvePartA())
+# IO.inspect(Day21.solvePartB())
