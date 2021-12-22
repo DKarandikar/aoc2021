6b8f0079139c20f61cbb967bb6cfbf5de705eccd
DKarandikar <daniel.karandikar@zoho.com>
2021-12-22 11:17:26 +0000

    Do day 22 part 1

--- src/day22.exs
+++ src/day22.exs
@@ -1,12 +1,43 @@
 defmodule Day22 do
   def getLines() do
     File.read!("input/day22.txt") |> String.split("\n")
+      |> Enum.map(fn x -> parseLine(x) end)
+  end
+
+  def parseLine(line) do
+    [state, rest] = String.split(line, " ")
+
+
+    [_ | captures] = Regex.run(~r/x=([-0-9]+)\.\.([-0-9]+)\,y=([-0-9]+)\.\.([-0-9]+)\,z=([-0-9]+)\.\.([-0-9]+)/, rest)
+    [xmin, xmax, ymin, ymax, zmin, zmax] = Enum.map(captures, &String.to_integer/1)
+
+    {(if state == "on", do: true, else: false), xmin, xmax, ymin, ymax, zmin, zmax}
+  end
+
+  def insideActivation?({_, xmin, xmax, ymin, ymax, zmin, zmax}) do
+    (xmin < 51 && xmax > -51) && (ymin < 51 && ymax > -51) && (zmin < 51 && zmax > -51)
   end
 
   def solvePartA() do
-    lines = Day22.getLines
-    lines |> hd
+    getLines()
+      |> Enum.filter(&insideActivation?/1)
+      |> Enum.reduce({MapSet.new(), MapSet.new()}, fn {turn_on, xmin, xmax, ymin, ymax, zmin, zmax}, {on, off} ->
+        to_turn = for x <- xmin..xmax, y <- ymin..ymax, z <- zmin..zmax, x < 51, x > -51, y < 51, y > -51, z < 51, z > -51, into: MapSet.new(), do: {x, y, z}
+        if turn_on, do: (
+          on = MapSet.union(on, to_turn)
+          off = MapSet.difference(off, to_turn)
+          {on, off}
+        ), else: (
+          off = MapSet.union(off, to_turn)
+          on = MapSet.difference(on, to_turn)
+          {on, off}
+        )
+
+      end)
+      |> count()
   end
+
+  def count({x, y}), do: {MapSet.size(x), MapSet.size(y)}
 
   def solvePartB() do
     lines = Day22.getLines
@@ -15,5 +46,5 @@ defmodule Day22 do
 
 end
 
-IO.puts(Day22.solvePartA())
-IO.puts(Day22.solvePartB())
+IO.inspect(Day22.solvePartA())
+# IO.puts(Day22.solvePartB())
