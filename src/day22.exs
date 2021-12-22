1c5a24e73f3da940a7694d3c0d814217e7e863dc
DKarandikar <daniel.karandikar@zoho.com>
2021-12-22 17:29:28 +0000

    Rework 22 part 1 solution to use part 2 code

--- src/day22.exs
+++ src/day22.exs
@@ -6,8 +6,6 @@ defmodule Day22 do
 
   def parseLine(line) do
     [state, rest] = String.split(line, " ")
-
-
     [_ | captures] = Regex.run(~r/x=([-0-9]+)\.\.([-0-9]+)\,y=([-0-9]+)\.\.([-0-9]+)\,z=([-0-9]+)\.\.([-0-9]+)/, rest)
     [xmin, xmax, ymin, ymax, zmin, zmax] = Enum.map(captures, &String.to_integer/1)
 
@@ -19,25 +17,21 @@ defmodule Day22 do
   end
 
   def solvePartA() do
-    getLines()
-      |> Enum.filter(&insideActivation?/1)
-      |> Enum.reduce({MapSet.new(), MapSet.new()}, fn {turn_on, xmin, xmax, ymin, ymax, zmin, zmax}, {on, off} ->
-        to_turn = for x <- xmin..xmax, y <- ymin..ymax, z <- zmin..zmax, x < 51, x > -51, y < 51, y > -51, z < 51, z > -51, into: MapSet.new(), do: {x, y, z}
-        if turn_on, do: (
-          on = MapSet.union(on, to_turn)
-          off = MapSet.difference(off, to_turn)
-          {on, off}
-        ), else: (
-          off = MapSet.union(off, to_turn)
-          on = MapSet.difference(on, to_turn)
-          {on, off}
-        )
+    lines = getLines()
+    {_, xmin, xmax, ymin, ymax, zmin, zmax} = lines |> hd
 
+    cubes = Enum.reduce(
+      lines |> tl,
+      MapSet.new([{xmin, xmax, ymin, ymax, zmin, zmax}]),
+      fn {turn_on, xmin, xmax, ymin, ymax, zmin, zmax}, cubes ->
+        if insideActivation?({false, xmin, xmax, ymin, ymax, zmin, zmax}), do: (
+          cubes = removeOverlap({xmin, xmax, ymin, ymax, zmin, zmax}, cubes)
+          if turn_on, do: MapSet.put(cubes, {xmin, xmax, ymin, ymax, zmin, zmax}), else: cubes
+        ), else: cubes
       end)
-      |> count()
-  end
 
-  def count({x, y}), do: {MapSet.size(x), MapSet.size(y)}
+    Enum.sum(Enum.map(cubes, fn {xmin, xmax, ymin, ymax, zmin, zmax} -> ((xmax + 1 - xmin) * (ymax + 1 - ymin) * (zmax + 1 - zmin))end))
+  end
 
   def removeOverlap({xmin, xmax, ymin, ymax, zmin, zmax}, cubes) do
     Enum.reduce(cubes, cubes, fn {xmin2, xmax2, ymin2, ymax2, zmin2, zmax2}, cubes ->
