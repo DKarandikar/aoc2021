b5399cd83bd0a11493b2604286498544a2d52ee7
DKarandikar <daniel.karandikar@zoho.com>
2021-12-22 17:14:46 +0000

    Solve day 22 part 2

--- src/day22.exs
+++ src/day22.exs
@@ -39,12 +39,48 @@ defmodule Day22 do
 
   def count({x, y}), do: {MapSet.size(x), MapSet.size(y)}
 
+  def removeOverlap({xmin, xmax, ymin, ymax, zmin, zmax}, cubes) do
+    Enum.reduce(cubes, cubes, fn {xmin2, xmax2, ymin2, ymax2, zmin2, zmax2}, cubes ->
+      minX = max(xmin, xmin2)
+      maxX = min(xmax, xmax2)
+      minY = max(ymin, ymin2)
+      maxY = min(ymax, ymax2)
+      minZ = max(zmin, zmin2)
+      maxZ = min(zmax, zmax2)
+
+      if (minX <= maxX) && (minY <= maxY) && (minZ <= maxZ), do: (
+        cubes = MapSet.delete(cubes, {xmin2, xmax2, ymin2, ymax2, zmin2, zmax2})
+        {cubes, xmin2} = if (xmin2 <= minX) && (minX <= xmax2), do: {MapSet.put(cubes, {xmin2, minX - 1, ymin2, ymax2, zmin2, zmax2}), minX}, else: {cubes, xmin2}
+        {cubes, xmax2} = if (xmin2 <= maxX) && (maxX <= xmax2), do: {MapSet.put(cubes, {maxX + 1, xmax2, ymin2, ymax2, zmin2, zmax2}), maxX}, else: {cubes, xmax2}
+
+        {cubes, ymin2} = if (ymin2 <= minY) && (minY <= ymax2), do: {MapSet.put(cubes, {xmin2, xmax2, ymin2, minY - 1, zmin2, zmax2}), minY}, else: {cubes, ymin2}
+        {cubes, ymax2} = if (ymin2 <= maxY) && (maxY <= ymax2), do: {MapSet.put(cubes, {xmin2, xmax2, maxY + 1, ymax2, zmin2, zmax2}), maxY}, else: {cubes, ymax2}
+
+        {cubes, zmin2} = if (zmin2 <= minZ) && (minZ <= zmax2), do: {MapSet.put(cubes, {xmin2, xmax2, ymin2, ymax2, zmin2, minZ - 1}), minZ}, else: {cubes, zmin2}
+        if (zmin2 <= maxZ) && (maxZ <= zmax2), do: MapSet.put(cubes, {xmin2, xmax2, ymin2, ymax2, maxZ + 1, zmax2}), else: cubes
+
+      ), else: cubes
+    end)
+  end
+
   def solvePartB() do
-    lines = Day22.getLines
-    lines |> hd
+    lines = getLines()
+    {_, xmin, xmax, ymin, ymax, zmin, zmax} = lines |> hd
+
+    cubes = Enum.reduce(
+      lines |> tl,
+      MapSet.new([{xmin, xmax, ymin, ymax, zmin, zmax}]),
+      fn {turn_on, xmin, xmax, ymin, ymax, zmin, zmax}, cubes ->
+        # Remove the overlap of new cubes with all old ones, and then add it if it's on
+        cubes = removeOverlap({xmin, xmax, ymin, ymax, zmin, zmax}, cubes)
+        if turn_on, do: MapSet.put(cubes, {xmin, xmax, ymin, ymax, zmin, zmax}), else: cubes
+    end)
+
+    Enum.sum(Enum.map(cubes, fn {xmin, xmax, ymin, ymax, zmin, zmax} -> ((xmax + 1 - xmin) * (ymax + 1 - ymin) * (zmax + 1 - zmin))end))
+
   end
 
 end
 
 IO.inspect(Day22.solvePartA())
-# IO.puts(Day22.solvePartB())
+IO.inspect(Day22.solvePartB())
